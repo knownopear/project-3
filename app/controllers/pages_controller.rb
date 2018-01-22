b9a3b73cabdf2e6344b29d5463ccac955d60eac1
Speed Chan <speedchanjun@gmail.com>
2018-01-22 09:19:31 +0800

    Speed's front page, some installation etc

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,32 +2,17 @@ class PagesController < ApplicationController
   def home
     # render 'pages/root'
   end
-# end
-
+  
   def about
   end
-# end
 
   def bookservices
   end
-# end
-
 
   def login
   end
-# end
 
 
   def signup
   end
 end
-#
-#
-# root 'pages#home' # get '/'
-#
-# get '/about', to: 'pages#about'
-# get '/bookservices', to: 'pages#bookservices'
-# get '/login', to: 'pages#login'
-# get '/signup', to: 'pages#signup'
-
-#I uncommented everything that was making this not work. When you have time, delete as you see fit. -Speed
