81a0f6dd261767ae8b24611f13e38ef4f913bff0
emjys <31798170+emjys@users.noreply.github.com>
2018-01-24 09:02:53 +0800

    devise added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,5 +20,16 @@
 
 $(document).on('turbolinks:load', function() {
   $('.parallax').parallax();
-  console.log('123');
+  $('ul.tabs').tabs();
+
+  $('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 300
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true, // Choose whether you can drag to open on touch screens,
+  });
+
+
+
+
 });
