df19f692458232ce8c4ecd33aaf0884a0398943d
emjys <31798170+emjys@users.noreply.github.com>
2018-01-31 18:00:35 +0800

    added text to thanks page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Therapist Name
+# Therapist Name
 10.times do |index|
   Therapist.create(
     name: Faker::Name.first_name
