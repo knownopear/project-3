649ef934ffb1c2bee6205ec61fcb5f4ae27b9a05
emjys <31798170+emjys@users.noreply.github.com>
2018-01-31 16:20:15 +0800

    update seed and ignore schema.rb;

--- db/migrate/20180128032844_create_join_table_bookings_services.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableBookingsServices < ActiveRecord::Migration[5.1]
-  def change
-    create_join_table :bookings, :services do |t|
-      # t.index [:booking_id, :service_id]
-      # t.index [:service_id, :booking_id]
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,6 +20,7 @@ Service.create(
   name: "Signature Massage",
   description: "A refreshing head and scalp massage including face acupressure and tension-releasing foot massage. Our specialty massage will leave your body rejuvenated from head to toe.",
   price: 180,
+  slots_taken: 2,
   therapist_id: Therapist.find(1).id
 )
 
@@ -28,6 +29,7 @@ Service.create(
   name: "Deep Tissue Massage",
   description: "Slow, deep pressure massage targets on stressed and tired muscles to unlock tension and relieve pain. An ideal pre or post workout massage to reduce injuries or recover faster.",
   price: 140,
+  slots_taken: 2,
   therapist_id: Therapist.find(2).id
 )
 
@@ -36,6 +38,7 @@ Service.create(
   name: "Time Saver Massage",
   description: "Gliding, kneading and pressure point movements concentrate on releasing tension in back, neck and shoulders. Ideal lunchtime stress relief or a quick stop on the way home from the office.",
   price: 60,
+  slots_taken: 2,
   therapist_id: Therapist.find(3).id
 )
 
@@ -44,6 +47,7 @@ Service.create(
   name: "Gentleman's Shave & Facial",
   description: "A precision shave with essential oil infused towel and customised facial which cleanses, hydrates and tones to effectively condition and address environmental exposure. Warm stones ease tension in neck and shoulders while leaving your face smooth and refreshed.",
   price: 120,
+  slots_taken: 2,
   therapist_id: Therapist.find(4).id
 )
 
@@ -52,6 +56,7 @@ Service.create(
   name: "Sensitive Skin Shave & Facial",
   description: "Redness and inflammation are calmed using soothing, naturally derived extracts without clogging pores to leave skin purified and hydrated. Restores  your skin and leaves it healthy.",
   price: 130,
+  slots_taken: 2,
   therapist_id: Therapist.find(5).id
 )
 
@@ -61,6 +66,7 @@ Service.create(
   name: "Traditional Shave",
   description: "Skin is prepared with pre-shave oil and lathered with hot shaving cream. Traditional straight razor with precision and mastery then a cold towel infused with lavender essential oil, finish with after-shave balm to hydrate the freshly shaven skin.",
   price: 50,
+  slots_taken: 2,
   therapist_id: Therapist.find(6).id
 )
 
@@ -69,6 +75,7 @@ Service.create(
   name: "Shampoo Blow Dry and Styling",
   description: "Signature shampoo and condition, including a relaxing scalp massage ensures the hair is clean healthy and protected. Finish off with a blow dry and styling to leave you ready to go for the day ahead.",
   price: 60,
+  slots_taken: 2,
   therapist_id: Therapist.find(7).id
 )
 
@@ -77,6 +84,7 @@ Service.create(
   name: "Customised Scalp Treatment",
   description: "Invigorating scalp massage utilizing a blend of essential oils to cleanse and relieve dry, itchy scalp. Continue with a hot towel and finish with rosemary shampoo, conditioner and hair styling of your choice.",
   price: 80,
+  slots_taken: 2,
   therapist_id: Therapist.find(8).id
 )
 
@@ -85,6 +93,7 @@ Service.create(
   name: "Therapeutic Mani-Pedicure",
   description: "Relieve tired hands and legs with bamboo granule exfoliation and a massage targeting muscle tension and stress. A moisturizer with natural essential oils is used to help promote circulation and healing.",
   price: 70,
+  slots_taken: 2,
   therapist_id: Therapist.find(9).id
 )
 
@@ -93,5 +102,6 @@ Service.create(
   name: "Time Saver Manicure",
   description: "Nail shaping, buffing, cuticle detailing and complete with an application of botanical lotion. Ideal for those on the go.",
   price: 30,
+  slots_taken: 2,
   therapist_id: Therapist.find(10).id
 )
